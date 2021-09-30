2fb13491b58ad21dbb3fd1370c83f0d675dc1eb9
Adam Dubey <adubey0311@gmail.com>
2021-09-30 13:52:09 -0400

    [AD][09M3021] Adding Firebase API deps and enabling userAuth flow

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { UserContext } from "..";
 import CreateList from "../components/CreateList";
 import Lists from "../components/Lists";
 import Layout from "../components/shared/Layout";
 
 function HomePage() {
+  const user = React.useContext(UserContext);
+
   return (
     <Layout>
-      <CreateList />
+      <CreateList user={user} />
       <Lists />
     </Layout>
   );
